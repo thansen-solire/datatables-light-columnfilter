f60123b71fe3089f035e53eef98784e70737c308
Raphaël <ramourette@intuitiv.fr>
2016-12-13 15:50:52 +0100

    Fix bootstrap3 styling

--- src/dataTables.lcf.bootstrap3.js
+++ src/dataTables.lcf.bootstrap3.js
@@ -29,14 +29,14 @@
       }
     );
 
-    ColumnFilter.filter.textBase = $.extend(true, {}, ColumnFilter.filter.text);
-    ColumnFilter.filter.text = {};
+    ColumnFilter.filter.inputBase = $.extend(true, {}, ColumnFilter.filter.input);
+    ColumnFilter.filter.input = {};
     $.extend(
-      ColumnFilter.filter.text,
-      ColumnFilter.filter.textBase,
+      ColumnFilter.filter.input,
+      ColumnFilter.filter.inputBase,
       {
         dom: function (th) {
-          ColumnFilter.filter.textBase.dom.call(this, th);
+          ColumnFilter.filter.inputBase.dom.call(this, th);
 
           this.elements.addClass('form-control input-sm');
 
@@ -45,53 +45,21 @@
       }
     );
 
-    ColumnFilter.filter.dateRangeBase = $.extend(true, {}, ColumnFilter.filter.dateRange);
-    ColumnFilter.filter.dateRange = {};
-    $.extend(ColumnFilter.filter.dateRange, ColumnFilter.filter.dateRangeBase, {
-      separator: '~',
-      dom: function(th) {
-          var self = this, input1, input2;
+    ColumnFilter.filter.rangeBase = $.extend(true, {}, ColumnFilter.filter.range);
+    ColumnFilter.filter.range = {};
+    $.extend(
+        ColumnFilter.filter.range,
+        ColumnFilter.filter.rangeBase,
+        {
+          dom: function (th) {
+            ColumnFilter.filter.rangeBase.dom.call(this, th);
 
-          this.div = $('<div>').addClass('input-group input-daterange');
-          input1 = $('<input>', {
-            type: 'text'
-          }).addClass('form-control input-sm').attr('name', 'start').appendTo(this.div);
+            this.elements.addClass('form-control input-sm');
 
-          input2 = $('<input>', {
-            type: 'text'
-          }).addClass('form-control input-sm').attr('name', 'end').appendTo(this.div);
-
-          this.elements = input1.add(input2);
-
-          this.div.appendTo(th);
-
-          return this.elements;
-      },
-      bindEvents: function () {
-        var self = this;
-
-        this.div.datepicker('clearDates').on('changeDate', function () {
-          self.search();
-        });
-      },
-      format: function (value) {
-        return value.split('/').reverse().join('-');
-      },
-      request: function () {
-        var
-                self = this,
-                search = []
-                ;
-
-        self.elements.each(function () {
-          var value = $(this).val();
-          value = self.options.format(value);
-          search.push(value);
-        });
-
-        return search.join(self.options.separator);
-      }
-    });
+            return this.elements;
+          }
+        }
+    );
   };
 
   // Define as an AMD module if possible
